707078b5dbcf371f811c29675d4ac0359a52ded2
mtrybus2208 <mtrybus2208@wp.pl>
2019-09-22 20:24:10 +0200

    Successfully connected to storyblok

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,59 +11,21 @@ exports.createPages = ({ graphql, actions }) => {
     resolve(
       graphql(
         `{
-          pages: allStoryblokEntry {
-            edges {
-              node {
-                id
-                name
-                created_at
-                published_at
-                uuid
-                slug
-                full_slug
-                content
-                is_startpage
-                parent_id
-                group_id
-                lang
-              }
-            } 
-          }
-
-          categories: allStoryblokEntry(filter: {field_component: {eq: "category"}}) {
-            edges {
-              node {
-                name
-                full_slug
-                slug
-              }
-            }
-          }
-
-          page22g: allStoryblokEntry(filter: {field_component: {eq: "page"}}) {
-            edges {
-              node {
-                name
-                slug
-                full_slug
-                content
-                is_startpage
-                parent_id
-              }
-            }
-          }
-          page22g: allStoryblokEntry(filter: {field_component: {eq: "page"}}) {
-            edges {
-              node {
-                name
-                slug
-                full_slug
-                content
-                is_startpage
-                parent_id
-              }
-            }
-          }
+          storyblok {
+            posts: PostItems(filter_query:{
+                      category: {
+                      in: "b58ee670-368f-477d-95b6-0cc380aa2b5d"
+                    }
+                }) {
+                  total
+                  items {
+                    content {
+                      title
+                      category
+                    }
+                  }
+                }
+           }
         }`
       )
       .then(result => {
@@ -71,51 +33,54 @@ exports.createPages = ({ graphql, actions }) => {
           console.log(result.errors);
           reject(result.errors);
         }
+        console.log(result.data.storyblok.posts.items[0].content);
         
-        const categories = result.data.categories.edges;
-        const entries = result.data.pages.edges;
-        const cats = result.data.page22g.edges;
-        console.log('cats');
-        console.log(cats);
-        ['en', 'pl'].forEach((lang) => {
-          createPage({
-            path: `/${lang}`,
-            component: homeEntry,
-            context: {
-              entries: entries,
-              lang,
-              categories, 
-            }
-          })
+        // const categories = result.data.categories.edges;
+        // const entries = result.data.pages.edges;
+        // const cats = result.data.cats.edges;
+        // console.log('cats');
+        // console.log(cats);
+        // ['en', 'pl'].forEach((lang) => {
+        //   createPage({
+        //     path: `/${lang}`,
+        //     component: homeEntry,
+        //     context: {
+        //       entries: entries,
+        //       lang,
+        //       categories, 
+        //     }
+        //   })
 
-          categories.forEach(cat => {
-            createPage({
-              path: `/${lang}/${cat.node.name}`,
-              component: categoryEntry,
-              context: {
-                entries: entries,
-                category: cat.node.name,
-                lang,
-                categories
-              }
-            })
-          });
-        })
+        //   categories.forEach(cat => {
+        //     createPage({
+        //       path: `/${lang}/${cat.node.name}`,
+        //       component: categoryEntry,
+        //       context: {
+        //         entries: entries,
+        //         category: cat.node.name,
+        //         lang,
+        //         categories
+        //       }
+        //     })
+        //   });
+        // })
  
-        entries.forEach((entry, index, array) => { 
-          let pagePath = entry.node.full_slug == 'home' ? '' : `${entry.node.full_slug}/`
-          console.log(pagePath);
-          createPage({
-            path: `/${pagePath}`,
-            component: storyblokEntry,
-            context: {
-              story: entry.node,
-              entries: array,
-              lang: entry.node.full_slug.split('/')[0],
-              categories
-            }
-          })
-        })
+        // entries.forEach((entry, index, array) => {
+        //   console.log('entry.node.full_slug');
+        //   console.log(entry.node.full_slug);
+        //   let pagePath = entry.node.full_slug == 'home' ? '' : `${entry.node.full_slug}/`
+        //   console.log(pagePath);
+        //   createPage({
+        //     path: `/${pagePath}`,
+        //     component: storyblokEntry,
+        //     context: {
+        //       story: entry.node,
+        //       entries: array,
+        //       lang: entry.node.full_slug.split('/')[0],
+        //       categories
+        //     }
+        //   })
+        // })
       })
     )
   })
